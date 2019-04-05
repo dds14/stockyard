07bb50982493fd4bf9540634555c78db4b98ed85
Derek Sams <dereksams@Dereks-MacBook-Pro.local>
2019-04-05 11:47:32 -0500

    update

---
+++ server/controllers/controller.js
@@ -0,0 +1,34 @@
+// change this, using template from node-day-2-mini
+// check out node-day-2-afternoon, it has the best info
+// node-day-2 is alright
+
+// let books = [];
+
+// let id = 0;
+
+// module.exports = {
+//     read: (req, res) => {
+//         res.status(200).send(books);
+//     },
+//     create: (req, res) => {
+//         const { title, author } = req.body;
+//         let book = {
+//             id: id,
+//             title: title,
+//             author: author
+//         }
+//         books.push(book);
+//         id++;
+//         res.status(200).send(books);
+//     }
+// }
+
+let favorites = []
+
+module.exports = {
+    addToFavorites: (req, res) => {
+        let favorite = req.body.name;
+        favorites.push(req.body.name, req.body.logo, req.body.ticker)
+        res.status(200).json(favorites)
+    }
+}
--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
-const express = require('express');
+const express = require('express'); // this is requiring express
 
-const app = express();
+const controller = require('./controllers/controller')
+
+
+const app = express(); // this is invoking express
 
 app.use(express.json()); // this is middleware
 
-app.get("api/changethis", bc.read) // change the bc. part
-app.post("api/changethis", bc.create) // change the bc. part
+// you could write the endpoints and stuff in here but you move it to a controller to organize the code
+
+app.post('/api/favorites', controller.addToFavorites)
 
 const PORT = 4000;
 app.listen(PORT, () => {
