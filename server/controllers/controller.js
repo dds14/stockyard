307950395963fb20e41a640c1cc02ebbc7b79ae2
Derek Sams <dereksams@Dereks-MacBook-Pro.local>
2019-04-10 18:16:59 -0500

    adding notes

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -2,26 +2,15 @@ let favorites = [];
 
 let notes = [];
 
-
 // READ (FAVORITES)
 const readFavorites = (req, res) => {
   res.status(200).json(favorites);
-}
+};
 
 // READ (NOTES)
 const readNotes = (req, res) => {
   res.status(200).json(notes);
-}
-
-// CREATE (NOTES)
-const addToNotes = (req, res) => {
-  req.body.id = notes.length + 1
-  notes.push(
-    req.body
-  )
-
-  res.status(200).json(notes)
-}
+};
 
 // CREATE (FAVORITES)
 const addToFavorites = (req, res) => {
@@ -33,22 +22,31 @@ const addToFavorites = (req, res) => {
   res.status(200).json(favorites);
 };
 
+// CREATE (NOTES)
+const addToNotes = (req, res) => {
+  req.body.id = notes.length + 1;
+  notes.push(req.body);
+
+  res.status(200).json(notes);
+};
+
 // UPDATE
 const updateNotes = (req, res) => {
   const index = notes.findIndex(note => {
-    return note.id === +req.params.id
-  })
-  notes[index].note = req.body.newNote
-  res.status(200).json(notes)
-}
+    return note.id === +req.params.id;
+  });
+  notes[index].note = req.body.newNote;
+  res.status(200).json(notes);
+};
 
 // DELETE
 const removeFavorites = (req, res) => {
   let name = req.params.name;
   favorites.splice(name, 1);
   res.send(favorites);
-}
+};
 
+// exporting the functions
 module.exports = {
   addToFavorites,
   readFavorites,
@@ -56,4 +54,4 @@ module.exports = {
   addToNotes,
   readNotes,
   updateNotes
-}
+};
--- server/index.js
+++ server/index.js
@@ -1,30 +1,43 @@
-const express = require("express"); // this is requiring express
+const express = require("express"); // this is requiring express - think of require as importing
 const app = express(); // this is invoking express
 
-const controller = require("./controllers/controller");
-const { addToFavorites, readFavorites, removeFavorites, addToNotes, readNotes, updateNotes } = require("./controllers/controller")
+// const controller = require("./controllers/controller");
+
+// this part is used so you don't have to put controller. before every endpoint (i.e. app/get("api/favorites", readFavorites))
+const {
+  addToFavorites,
+  readFavorites,
+  removeFavorites,
+  addToNotes,
+  readNotes,
+  updateNotes
+} = require("./controllers/controller");
 
 app.use(express.json()); // this is middleware
 
 // READ (FAVORITES)
-app.get("/api/favorites", controller.readFavorites);
+app.get("/api/favorites", readFavorites);
+// the get function takes in two arguments (1) the url (/api/favorites)
+// and (2) the function that tells express what to send back to
+// the person making the request (readFavorites)
 
 // READ (NOTES)
-app.get("/api/notes", readNotes)
-
-// CREATE (NOTES)
-app.post("/api/notes", addToNotes);
+app.get("/api/notes", readNotes);
 
 // CREATE (FAVORITES)
 app.post("/api/favorites", addToFavorites);
 
+// CREATE (NOTES)
+app.post("/api/notes", addToNotes);
 
 // UPDATE
 app.put("/api/notes/:id", updateNotes);
 
 // DELETE
-app.delete("/api/favorites/:name", removeFavorites)
+app.delete("/api/favorites/:name", removeFavorites);
 
+// setting the port number and telling the server to listen for it
+// it binds the app to the port that we specify
 const PORT = 4041;
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
